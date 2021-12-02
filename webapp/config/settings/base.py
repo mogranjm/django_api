e5334ff6881507124fdfd944dba911b17d1ed627
James Morgan <ja.morgan1@outlook.com>
2021-12-02 17:49:55 +1100

    Remove secret key from version control

--- webapp/config/settings/base.py
+++ webapp/config/settings/base.py
@@ -9,23 +9,26 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Env
+ENV_FILE = BASE_DIR / '.env'
+env = environ.Env()
+if Path(ENV_FILE).exists():
+    env.read_env(ENV_FILE)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xgzjv0$k0d_)*hgtddnsvlkgy!95lr-uvdf$dxscq-m=e70s^%'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
---
+++ webapp/config/settings/local.py
@@ -0,0 +1,5 @@
+from .base import *
+from .base import env
+
+
+SECRET_KEY = env('DJANGO_SECRET KEY')
